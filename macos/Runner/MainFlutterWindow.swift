380eef408a0b7a5ef7aeaad3e0255635ddadf165
Kaka <kanniu@163.com>
2022-03-16 12:48:13 +0800

    set fixed window size in macos platform

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -7,6 +7,9 @@ class MainFlutterWindow: NSWindow {
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.setContentSize(NSSize(width: 375,height: 667))
+    let window: NSWindow! = self.contentView?.window
+    window.styleMask.remove(.resizable)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
